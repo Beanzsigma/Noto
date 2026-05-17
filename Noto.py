b06ebbed27f7996e6b769781b6623169792c908e
Beanzsigma <162669334+Beanzsigma@users.noreply.github.com>
2026-05-17 16:51:43 -0700

    Added Summarize button

--- Noto.py
+++ Noto.py
@@ -53,10 +53,24 @@ def main():
     canvas, canvasbg = gifbg()
     canvas.create_text(304, 34, text="Noto", font=("Khuja Uppercase Uppercase", 37), fill="#585454", anchor='center')
     canvas.create_text(300, 30, text="Noto", font=('Khuja Uppercase Uppercase', 37), fill="#bbb5b5", anchor="center")
-    notebox = ctk.CTkTextbox(app, width=500, height=200, fg_color="#111111", text_color="white", border_color="white", border_width=1, corner_radius=8, font=("OriginalMagnolia", 14), wrap='word')
+    notebox = ctk.CTkTextbox(app, width=500, height=225, fg_color="#111111", text_color="white", border_color="white", border_width=1, corner_radius=8, font=("OriginalMagnolia", 14), wrap='word')
     notebox.place(x=50, y=80)
-    summarybox = ctk.CTkTextbox(app, width=500, height=150, fg_color="#111111", text_color='white')
-
+    summarybox = ctk.CTkTextbox(app, width=500, height=188, fg_color="#111111", text_color='white', border_color="white", border_width=1, corner_radius=8, font=('OriginalMagnolia', 14), wrap='word')
+    summarybox.place(x=50, y=385)
+    submitshdw = canvas.create_text(303, 353, text="Summarize", font=('Khuja Uppercase Uppercase', 24), fill="#585454", anchor='center')
+    submit = canvas.create_text(300, 350, text="Summarize", font=('Khuja Uppercase Uppercase', 24), fill="#bbb5b5", anchor='center')
+    def enter(e):
+        canvas.itemconfig(submitshdw, fill="#0a0a0a")
+        canvas.itemconfig(submit, fill="#585454")
+    def leave(e):
+        canvas.itemconfig(submit, fill="#bbb5b5")
+        canvas.itemconfig(submitshdw, fill="#585454")
+    canvas.tag_bind(submit, "<Enter>", enter)
+    canvas.tag_bind(submitshdw, "<Enter>", enter)
+    canvas.tag_bind(submitshdw, "<Leave>", leave)
+    canvas.tag_bind(submit, "<Leave>", leave)
+    canvas.tag_bind(submitshdw, "<Button-1>", main)
+    canvas.tag_bind(submit, "<Button-1>", main)
 
 main()
 
