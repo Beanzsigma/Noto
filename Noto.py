02f43ba63ca46b6d2f914c33111a2dc5a540a8cd
Beanzsigma <162669334+Beanzsigma@users.noreply.github.com>
2026-05-17 15:49:23 -0700

    Created window

---
+++ Noto.py
@@ -0,0 +1,24 @@
+import customtkinter as ctk
+import sounddevice as sd
+from groq import Groq
+from tkinter import Canvas, Text
+import pythoncom 
+app = ctk.CTk()
+app.title("Noto")
+app.geometry('400x400')
+app.resizable(False, False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.mainloop()
